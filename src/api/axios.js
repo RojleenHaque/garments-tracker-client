1a1f2f7db5753ce4cdf36748a6a4aba58d047932
Abstract Progermmer <AbstractProgermmer@example.com>
2025-12-24 20:51:16 +0600

    fixed booking issue6

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://garments-tracker-server-sooty.vercel.app',
-  withCredentials: true,
+  baseURL: 'https://garments-tracker-server-cgog.vercel.app',
+  withCredentials: true, // if you use cookies for auth
 });
 
-
 export default api;
+
+

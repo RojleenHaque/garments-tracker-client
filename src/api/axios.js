f7eae2bd74bed840addfd88ce2c7b1d8dec17174
Abstract Progermmer <AbstractProgermmer@example.com>
2026-05-12 02:40:05 +0600

    trying render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://garments-tracker-server-zeta.vercel.app",
+  baseURL: "https://garments-tracker-server.onrender.com",
   withCredentials: true,
 });
 
